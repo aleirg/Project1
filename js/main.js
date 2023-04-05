67201e7b418f3d1b3a2ea4496a36e96f2e0fccf5
aleirg <aargbl5@gmail.com>
2023-04-05 00:26:08 -0500

    declare variables and cache els

--- js/main.js
+++ js/main.js
@@ -1,14 +1,39 @@
-  /*----- constants -----*/
-  
+/*----- constants -----*/
+// object of colors
+const colors = {
+  null: 'white',
+  1: '',
+  2: '',
+  3: '',
+  4: '',
+};
+
+// audio variables 
+// const audio1;
+// const audio2;
+// const audio3;
+// const audio4;
+
+// audio and color pairs for sequence
+// const audioColorSeq;
 
 
-  /*----- state variables -----*/
+/*----- state variables -----*/
+let gameBoard;
+let gameSequence;
+let playerSequence;
+let statusOfGame;
 
 
-  /*----- cached elements  -----*/
+/*----- cached elements  -----*/
+// get circleBoard divs
+
+// buttons 
+const startGameClick = document.getElementById('startBtn');
+const newGameClick = document.getElementById('newGameBtn');
 
 
-  /*----- event listeners -----*/
+/*----- event listeners -----*/
 
 
-  /*----- functions -----*/
+/*----- functions -----*/
